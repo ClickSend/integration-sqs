80a049c525615c5fd8f3c61a7e2cff9b036f399b
Sourav Gupta <souravgupta@192-168-1-129.tpgi.com.au>
2021-12-16 12:39:29 +1100

    Updated code for multiple recipients

--- producer/java/src/main/java/Main.java
+++ producer/java/src/main/java/Main.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.List;
 import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
 import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
@@ -5,45 +7,58 @@ import software.amazon.awssdk.services.sqs.SqsClient;
 import software.amazon.awssdk.services.sqs.model.SendMessageRequest;
 import software.amazon.awssdk.services.sqs.model.SendMessageResponse;
 import software.amazon.awssdk.auth.credentials.ProfileCredentialsProvider;
-   
-
 
 public class Main {
 
-    // Insert your AWS queue url
-    private final static String QUEUE_URL ="https://sqs.region.amazonaws.com/XXXXXXXXXXXX/queuename";//
+    //change with your own queue url
+    private final static String QUEUE_URL ="https://sqs.region.amazonaws.com/XXXXXXXXXXXX/queuename";
 
     static ProfileCredentialsProvider acc = ProfileCredentialsProvider.builder().build();
 
-
-    public static void main(String[] args) {
-        
-        // Reads your AWS credentials from credentials file: Access_key_ID, Access_key_secret
-        AwsBasicCredentials awsCreds = AwsBasicCredentials.create(
+    // Reads your AWS credentials from credentials file: Access_key_ID, Access_key_secret
+    static AwsBasicCredentials awsCreds = AwsBasicCredentials.create(
                 acc.resolveCredentials().accessKeyId(),
                 acc.resolveCredentials().secretAccessKey());
 
-        // Insert the your region, for information follow: https://docs.aws.amazon.com/AmazonRDS/latest/UserGuide/Concepts.RegionsAndAvailabilityZones.html
-        SqsClient sqsClient = SqsClient.builder()
-                .region(Region.region)
-                .credentialsProvider(StaticCredentialsProvider.create(awsCreds))
-                .build();
+    public static SendMessageResponse sendMessage(String phoneNumber, String message, SqsClient sqsClient){
 
         SendMessageResponse response= sqsClient.sendMessage(SendMessageRequest.builder()
                 .queueUrl(QUEUE_URL)
 
-        // Your message body:- 
-        // to: reciever number, 
-        // body: your message, 
+    // Your message body:- 
+        // to: reciever number, use the "numbers" list to change the recipients
+        // body: your message, use the "messageToSend" string to change the content of message
         // from: by default it is shared number, you can also choose your business name, or your own number,
         // for more information regarding "from" follow: https://help.clicksend.com/article/4kgj7krx00-what-is-a-sender-id-or-sender-number
                 .messageBody("{" +
-                        "\"to\":\"+6311111111\"," +
-                        "\"source\":\"sqs\"," +
-                        "\"body\":\"hello from java\"," +
-                        "\"from\":\"Testing\""+
-                        "}")
+                        "\"to\":\""+phoneNumber+"\"," +
+                        "\"source\":\"SQS\"," +
+                        "\"body\":\""+message+"\","+
+                        "\"from\":\"Testing\""+"}")
                 .build());
-        System.out.println(response);
-    } 
+        return response;
+    }
+
+    public static void main(String[] args) {
+ 
+        // Insert the your region, for information follow: https://docs.aws.amazon.com/AmazonRDS/latest/UserGuide/Concepts.RegionsAndAvailabilityZones.html
+        SqsClient sqsClient = SqsClient.builder()
+                .region(Region.AP_SOUTHEAST_2)
+                .credentialsProvider(StaticCredentialsProvider.create(awsCreds))
+                .build();
+
+        List<String> numbers = new ArrayList<>();
+
+        // Add the recipient's numbers
+        numbers.add("+111111111");
+        numbers.add("+505555555");
+        numbers.add("+606666666");
+
+        // Your message
+        String messagesToSend = "Hello from AWS SQS java";
+
+        for(String number : numbers){
+            System.out.println(sendMessage(number,messagesToSend,sqsClient));
+        }
+    }
 }
--- producer/nodejs/main.js
+++ producer/nodejs/main.js
@@ -1,27 +1,41 @@
-// Load the AWS SDK for Node.js
+// Loads the AWS SDK for Node.js
 var AWS = require('aws-sdk');
-// Set the region 
+// Sets the region 
 AWS.config.update({region: 'Region'});
 
 // Create an SQS service object
 var sqs = new AWS.SQS({apiVersion: '2012-11-05'});
 
-var params = {
-  // Your message body:- 
-        // to: reciever number, 
-        // body: your message, 
+
+function sendMessage(to, message){
+	var params = {
+	// Your message body:- 
+        // to: reciever number, use "numbers" list to add the recipients
+        // body: your message, use "messageToSend" variable to change the content of message
         // from: by default it is shared number, you can also choose your business name, or your own number,
         // for more information regarding "from" follow: https://help.clicksend.com/article/4kgj7krx00-what-is-a-sender-id-or-sender-number
-  MessageBody: '{"to":"+6111111111","source":"sqs","body":"hello from node", "from":"Testing"}',
+		MessageBody: '{"to":"'+to+'","source":"SQS","body":"'+message+'","from":"Testing"}',
 
-  // Insert your AWS queue url
-  QueueUrl: "https://sqs.region.amazonaws.com/XXXXXXXXXXXX/queuename"
-};
+		// Insert your AWS queue url
+		QueueUrl: "https://sqs.region.amazonaws.com/XXXXXXXXXXXX/queuename"
+	};
 
-sqs.sendMessage(params, function(err, data) {
-  if (err) {
-    console.log("Error", err);
-  } else {
-    console.log("Success", data.MessageId);
-  }
+	sqs.sendMessage(params, function(err, data) {
+		if (err) {
+		console.log("Error", err);
+	} else {
+		console.log("Success", data.MessageId);
+	}
 });
+}
+
+// Add the recipient's numbers
+var numbers = ["+606666666","+505555555","+111111111"];
+// Your message
+var messageToSend = "Hello from AWS SQS NodeJS";
+
+numbers.forEach(sendSmsToAll);
+
+function sendSmsToAll(number) {
+  sendMessage(number,messageToSend);
+}
